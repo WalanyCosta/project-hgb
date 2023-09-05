dabd50b4b73a9f03c801ff3554b40c99d3de34eb
Walany Costa <walanybnegro@gmail.com>
2023-09-05 13:21:30 +0100

    criado o modal e também a chamada do modal

--- render-views.mjs
+++ render-views.mjs
@@ -6,8 +6,4 @@ renderViews.get('/', async (request, response)=>{
     response.render('home')    
 })
 
-renderViews.get('/formulario', async (request, response)=>{
-    response.render('modal-formulario') 
-})
-
 export default renderViews
