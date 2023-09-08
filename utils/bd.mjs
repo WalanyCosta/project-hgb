75e4764c27fe213f9dc903d70a0cbdcd75cb011f
Walany Costa <walanybnegro@gmail.com>
2023-09-08 10:39:35 +0100

    criado o convertor de html para pdf

---
+++ utils/bd.mjs
@@ -0,0 +1,9 @@
+export const BANCODADOMEMORIA = [
+    {
+        nome: 'Fungi Teka',
+        numeroAgente: '0000LA2345',
+        curso: 'Enfermagem',
+        data_nasc: '2023-08-09',
+        BI: '0000000000000LA'
+    }    
+]
