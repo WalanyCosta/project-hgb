fa91d5ba6c768d28141cbef83769f0e4ceba3427
Walany Costa <walanybnegro@gmail.com>
2023-09-08 18:25:53 +0100

    adicionado loading e alert do bootstrap para indicar a demora do processo e alert para mostrar sucesso ou falha

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,27 +1,64 @@
 const numeroAgente = document.getElementById('CIF')
+const butaoGerarCertificado = document.getElementById('gerador')
 const forms = document.querySelectorAll('.needs-validation')
+const alertMensagem = document.getElementById('alertMensagem')
+
+const inserirLoading = () => {
+    butaoGerarCertificado.innerHTML = `
+        <div class="spinner-border spinner-border-sm" role="status">
+            <span class="visually-hidden">Loading...</span>
+        </div>
+    `
+}
+
+const adicionarAlertMensagem = (message, type) => {
+    if(alertMensagem?.children.length === 1){
+        alertMensagem.innerHTML = ''
+    }
+
+    const divWrapper = document.createElement('div')
+    divWrapper.innerHTML = [
+      `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+      `   <div>${message}</div>`,
+      '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+      '</div>'
+    ].join('')
+  
+    alertMensagem.append(divWrapper)
+  }
+
+const gerarCertificado = () => {
+
+    inserirLoading()
+
+    const resposta = fetch(`http://localhost:3333/certificado/${numeroAgente.value}`, {
+        method: 'GET',
+        mode: 'cors'
+    })
+        .then(response => response.json())
+        .then(data => {
+            if(data?.messagemError) adicionarAlertMensagem(data.messagemError, 'danger') 
+            adicionarAlertMensagem(data.mensagem, 'success')
+        })
+        .catch(error => error.mensagem)
+        .finally(()=> {
+            butaoGerarCertificado.innerHTML = `Gerar Certificado`
+        })
+    return resposta
+}
+
 
 Array.from(forms).forEach(form => {
   form.addEventListener('submit', event => {
     event.preventDefault()
+    
     if (!form.checkValidity()) {
       event.stopPropagation()
     }
 
     form.classList.add('was-validated')
 
-    fetch(`http://localhost:3333/certificado/${numeroAgente.value}`, {
-        method: 'GET',
-        mode: 'cors'
-    })
-        .then(response => response.json())
-        .then(data => {
-            
-            if(data?.messagemError) alert(data.messagemError)
-
-            alert(data.mensagem)
-        })
-        .catch(error => {console.log(error.mensagem)})
-
+    gerarCertificado()
+  
   })
 })
